7b4018d577e4a7f4059b35c0c4d36413bdd479cd
Laryna Billinghurst <Lunchbox1990@hotmail.com>
2019-04-11 14:57:49 -0700

    crud-and-jest-examples-success-starting-cats-table

--- api/server.js
+++ api/server.js
@@ -1,11 +1,23 @@
+//express for server
 const express = require('express');
-
+//routes
+const hobbits = require('../alphatestDB/hobbits/hobbitsModel.js')
+//server uses express
 const server = express();
-
+//middleware
 server.use(express.json());
-
+//routing
+//ROOT
 server.get('/', async (req, res) => {
   res.status(200).json({ api: 'up' });
 });
+//HOBBITS
+server.get('/hobbits', async (req, res) => {
+  const rows = await hobbits.getAll();
+
+  res.status(200).json(rows);
+});
+//CATS
+
 
 module.exports = server;
--- knexfile.js
+++ knexfile.js
@@ -2,13 +2,26 @@
   module.exports = {
     development: {
       client: 'sqlite3',
-      connection: { filename: './database/test.sqlite3' }, // change this if you want a different name for the database
+      connection: { filename: './database/alphatest.sqlite3' }, // change this if you want a different name for the database
       useNullAsDefault: true, // used to avoid warning on console
       migrations: {
         directory: './database/migrations',
         tableName: 'dbmigrations',
       },
       seeds: { directory: './database/seeds' },
+    },
+    testing: {
+      client: 'sqlite3',
+      connection: {
+        filename: './database/test.db3',
+      },
+      useNullAsDefault: true,
+      migrations: {
+        directory: './database/migrations',
+      },
+      seeds: {
+        directory: './database/seeds',
+      },
     },
   };
   
