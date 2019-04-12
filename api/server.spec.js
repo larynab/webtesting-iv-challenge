6872a926e9b0020d30ea2857cdd54371860ecfdb
Laryna Billinghurst <Lunchbox1990@hotmail.com>
2019-04-11 18:54:12 -0700

    rewinding-steps-to-understand-material

--- api/server.spec.js
+++ api/server.spec.js
@@ -1,61 +1,104 @@
+//testing endpoints
+//returns correct http status code
+//supertest for these tests
 const request = require('supertest');
-const server = require('./server.js');
+//bring in server js for cruds-responses
+const server = require('../api/server.js');
+
+//SERVER
+describe('server.js', () => {
+  describe('GET root /', () => {
+    it('should respond with 200 ok', () => {
+      //requesting from imported server.js, return to wait for promise
+      return request(server)
+      //get the endpoint
+      .get('/')
+      //response from request 
+      .then(response => {expect(response.status).toBe(200)});
+    })
+  })
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//GUIDED DEMO-------------------------------------------------------------------------------
+// const request = require('supertest');
+// const server = require('./server.js');
 
 // testing endpoints
 // returns correct http status code
 
-describe('server.js', () => {
-  describe('GET /', () => {
-    it('should respond with 200 OK', () => {
-      return request(server)
-        .get('/')
-        .then(response => {
-          expect(response.status).toBe(200);
-        });
-    });
+// describe('server.js', () => {
+//   describe('GET /', () => {
+//     it('should respond with 200 OK', () => {
+//       return request(server)
+//         .get('/')
+//         .then(response => {
+//           expect(response.status).toBe(200);
+//         });
+//     });
 
-    it('should respond with 200 OK async', async () => {
-      const response = await request(server).get('/');
+//     it('should respond with 200 OK async', async () => {
+//       const response = await request(server).get('/');
 
-      expect(response.status).toBe(200);
-    });
+//       expect(response.status).toBe(200);
+//     });
 
-    it('should respond with 200 OK async', done => {
-      request(server)
-        .get('/')
-        .then(response => {
-          expect(response.status).toBe(200);
+//     it('should respond with 200 OK async', done => {
+//       request(server)
+//         .get('/')
+//         .then(response => {
+//           expect(response.status).toBe(200);
 
-          done();
-        });
-    });
+//           done();
+//         });
+//     });
 
-    it('should work', () => {
-      return request(server)
-        .get('/')
-        .expect(200);
-    });
+//     it('should work', () => {
+//       return request(server)
+//         .get('/')
+//         .expect(200);
+//     });
 
-    it('should return JSON', () => {
-      return request(server)
-        .get('/')
-        .then(res => {
-          expect(res.type).toBe('application/json');
-        });
-    });
-  });
+//     it('should return JSON', () => {
+//       return request(server)
+//         .get('/')
+//         .then(res => {
+//           expect(res.type).toBe('application/json');
+//         });
+//     });
+//   });
 
-  it('should check for json', () => {
-    return request(server)
-      .get('/')
-      .expect('Content-Type', /json/);
-  });
+//   it('should check for json', () => {
+//     return request(server)
+//       .get('/')
+//       .expect('Content-Type', /json/);
+//   });
 
-  it('should return { api: "up" }', () => {
-    return request(server)
-      .get('/')
-      .then(res => {
-        expect(res.body).toEqual({ api: 'up' });
-      });
-  });
-});
+//   it('should return { api: "up" }', () => {
+//     return request(server)
+//       .get('/')
+//       .then(res => {
+//         expect(res.body).toEqual({ api: 'up' });
+//       });
+//   });
+// });
